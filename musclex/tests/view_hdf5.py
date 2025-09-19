ce213350f4627cb904c0bc7ebf21b1f136944afc
Alex <alexlanxy@gmail.com>
2025-09-19 10:13:29 -0500

    Revert "Add new HDF5 test datasets and implement image inversion feature in GUI"
    This reverts commit c5f5b13b4efb4b656750c5d7bc6db60fce26320c.

--- musclex/tests/view_hdf5.py
+++
@@ -1,4 +0,0 @@
-import h5py, matplotlib.pyplot as plt
-with h5py.File("/Users/alex/VSCode/musclex/musclex/tests/mnist-train.hdf5", "r") as f:
-    img = f["/image"][5]  # change 0 to any index 0..59999
-plt.imshow(img, cmap="gray"); plt.axis("off"); plt.show()
